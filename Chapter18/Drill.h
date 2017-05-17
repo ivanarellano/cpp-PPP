7efc47c9bb8e310726e96d54a06e03c4fa40dad0
Ivan <ia@ivanarellano.com>
2017-05-16 21:44:50 -0400

    Cleanup

--- Chapter18/Drill.h
+++ Chapter18/Drill.h
@@ -4,6 +4,15 @@
 
 int ga[]{ 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
+int factorial(int n)
+{
+	if (1 > n) throw std::exception("input must be greater than 1");
+
+	int factor = 1;
+	for (int i = 1; i <= n; ++i) factor *= i;
+	return factor;
+}
+
 void f(int arr[], int sz)
 {
 	int la[10];
@@ -43,4 +52,36 @@ void f(const std::vector<int>& v)
 
 	for (auto i : lv2)
 		std::cout << i << " ";
+}
+
+void drill()
+{
+	std::cout << "Start array drill" << std::endl;
+
+	f(ga, 10);
+	std::cout << std::endl << std::endl;
+
+	int aa[10];
+
+	for (int i = 0; i < 10; ++i)
+		aa[i] = factorial(i + 1);
+
+	f(aa, 10);
+
+	std::cout
+		<< std::endl << std::endl
+		<< "Start vector drill"
+		<< std::endl;
+
+	f(gv);
+	std::cout << std::endl << std::endl;
+
+	std::vector<int> vv(10);
+	int i = 0;
+	for (auto it = vv.begin(); it != vv.end(); ++it, ++i)
+		*it = factorial(i + 1);
+
+	f(vv);
+
+	std::cout << std::endl;
 }
--- Chapter18/main.cpp
+++ Chapter18/main.cpp
@@ -2,47 +2,11 @@
 #include "Chapter.h"
 #include "Drill.h"
 
-int factorial(int n)
-{
-	if (1 > n) throw std::exception("input must be greater than 1");
-
-	int factor = 1;
-	for (int i = 1; i <= n; ++i) factor *= i;
-	return factor;
-}
-
 int main()
 {
 	//chapter();
 
-	std::cout << "Start array drill" << std::endl;
-
-	f(ga, 10);
-	std::cout << std::endl << std::endl;
-
-	int aa[10];
-
-	for (int i = 0; i < 10; ++i)
-		aa[i] = factorial(i + 1);
-	
-	f(aa, 10);
-
-	std::cout 
-		<< std::endl << std::endl
-		<< "Start vector drill"
-		<< std::endl;
-
-	f(gv);
-	std::cout << std::endl << std::endl;
-
-	std::vector<int> vv(10);
-	int i = 0;
-	for (auto it = vv.begin(); it != vv.end(); ++it, ++i)
-		*it = factorial(i + 1);
-
-	f(vv);
-
-	std::cout << std::endl;
+	drill();
 
 	keep_window_open();
 
