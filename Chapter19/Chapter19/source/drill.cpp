4bda923da4e3339ebc60bd8a4c178d8fd5a34a82
Ivan <hubbu09@gmail.com>
2017-06-11 22:00:38 -0400

    Continue Drill work

--- Chapter19/Chapter19/source/drill.cpp
+++ Chapter19/Chapter19/source/drill.cpp
@@ -1,6 +1,7 @@
 #include "work_selection.h"
 #include <string>
 #include <iostream>
+#include <vector>
 #include "Utils.h"
 
 /// 1
@@ -9,15 +10,64 @@ struct S {
 	/// 2
 	S(T val) : val{val} {}
 
-	T val;
-
 	/// 5
-	T get() const;
+	const T& get() const;
+	/// 11
+	T& get() { return val; }
+
+	/// 9
+	T set(const T& new_val) { val = new_val; }
+
+	/// 10
+	T& operator=(const T&);
+
+/// 7
+private:
+	T val;
 };
 
+/// 6
 template<typename T>
-T S<T>::get() const {
+const T& S<T>::get() const {
 	return val;
+}
+
+/// 10
+template<typename T>
+T& S<T>::operator=(const T& other) {
+	val = other;
+	return val;
+}
+
+/// 12
+template<typename T>
+std::istream& operator>>(std::istream& is, S<T>& ss) {
+	T v;
+	std::cin >> v;
+	if (!is) return is;
+	ss = v;
+	return is;
+}
+
+template<typename T>
+void read_val(T& v) {
+	std::cout << "Enter a value\n"; 
+	std::cin >> v;
+}
+
+/// 14
+template<typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> vec) {
+	os << "{ ";
+
+	for (int i = 0; i < vec.size(); ++i) {
+		os << vec[i];
+		if (i < vec.size() - 1) os << ",";
+		os << " ";
+	}
+
+	os << "}";
+	return os;
 }
 
 int Drill::main() {
@@ -26,14 +76,22 @@ int Drill::main() {
 	S<char> my_char{ S<char>('a') };
 	S<double> my_double(1.f);
 	S<std::string> my_string = "my string";
+	S<std::vector<int>> my_vect{ std::vector<int>{1, 3} };
 
-	/// 4
-	std::cout << my_int.val << std::endl
-		<< my_char.val << std::endl
-		<< my_double.val << std::endl
-		<< my_string.val << std::endl;
+	/// 4, then 8
+	std::cout << my_int.get() << std::endl
+		<< my_char.get() << std::endl
+		<< my_double.get() << std::endl
+		<< my_string.get() << std::endl;
+	std::cout << my_vect.get()[0] << my_vect.get()[1] << std::endl;
 
+	read_val(my_int);
+	read_val(my_char);
+	read_val(my_double);
 
+	std::cout << my_int.get() << std::endl
+		<< my_char.get() << std::endl
+		<< my_double.get() << std::endl;
 
 	keep_window_open();
 	return 0;
