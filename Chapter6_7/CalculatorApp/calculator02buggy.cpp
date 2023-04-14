9d7cde6e6464d640a38e1a9c583af97149522388
Ivan <ia@ivanarellano.com>
2023-04-13 20:32:20 -0400

    Ch6 Ex2

--- Chapter6_7/CalculatorApp/calculator02buggy.cpp
+++ Chapter6_7/CalculatorApp/calculator02buggy.cpp
@@ -81,7 +81,7 @@ Token Token_stream::get()
     switch (ch) {
     case PrintChar:    // for "print"
     case ExitChar:    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/':
+    case '{': case '}': case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
@@ -112,6 +112,13 @@ double primary()
 {
     Token t = ts.get();
     switch (t.kind) {
+    case '{':    // handle '{' expression '}'
+    {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != '}') error("'}' expected");
+        return d;
+    }
     case '(':    // handle '(' expression ')'
     {
         double d = expression();
